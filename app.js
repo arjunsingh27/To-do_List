97b6f83ad752b1fd3e4613d0dc75dac95695a79d
ArjunSingh <70803514+arjunsingh27@users.noreply.github.com>
2023-07-24 14:21:51 +0530

    IF Mongoose.Connect

--- app.js
+++ app.js
@@ -4,15 +4,13 @@ const mongoose = require("mongoose");
 const ejs = require("ejs");
 const app = express();
 
-mongoose.connect("mongodb+srv://arjunsingh27:Test123@cluster0.0t9vaxx.mongodb.net/todoList");
- 
-
-
-
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+mongoose.connect("mongodb+srv://arjunsingh27:Test123@cluster0.0t9vaxx.mongodb.net/todoList")
+  .then(() => {
+    
 
 
 app.get("/", function (req, res) {
@@ -91,3 +89,10 @@ const defaultcollection = [item1, item2, item3];
 app.listen(process.env.PORT || 3000,function(req,res){
     console.log("Server Started .........");
 })
+
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+
+
